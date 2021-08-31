f1efb6d511ec13b3034c4469792dc7824534693a
Coy Reid <coyreid7@gmail.com>
2021-08-31 17:50:07 -0500

    commit tree 990fb471313222682d5d9e9a24979f9f33a4ad74

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  # your code here
+  
+  def initialize(attr = nil)
+    if attr
+      attr.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
+    end
+  end
+
 end
